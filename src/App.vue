9d01f852519907367a600bbfa726c9fe066edca8
hesham <heshamelmasry1992@gmail.com>
2020-01-23 16:08:52 +0200

    add style sheet external, add new font  from google, configure the eslint to allow indentation as my IDE

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <SchoolForm/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+  import SchoolForm from './components/SchoolForm.vue';
+  import './App.scss';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld,
-  },
-};
+  export default {
+    name: 'app',
+    components: {
+      SchoolForm,
+    },
+  };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
