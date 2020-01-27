8d8e9258ff2e9f5975f1ba0b3a4ea6889778e95b
hesham <heshamelmasry1992@gmail.com>
2020-01-27 17:26:47 +0200

    new changes on the form

--- src/components/SchoolForm.vue
+++ src/components/SchoolForm.vue
@@ -26,37 +26,40 @@
 
                     <b-form-checkbox
                             id="checkbox-allow-download-for-students" class="text-left"
-                            v-model="form.allowDownloadForStudents"
+                            v-model="form.can_students_download"
                             name="checkbox-allow-download-for-students"
                             value="true"
                             unchecked-value="not_accepted"
                     >
                         Allow Download For Students
                     </b-form-checkbox>
-                    <b-form-textarea
-                            id="video-description"
-                            v-model="form.description"
-                            placeholder="Enter description for video..."
-                            rows="3"
-                            max-rows="6"
-                    ></b-form-textarea>
+
+                    <b-form-checkbox
+                            id="publish-timeline" class="text-left"
+                            v-model="form.publish_timeline"
+                            name="publish-timeline"
+                            value="true"
+                            unchecked-value="not_accepted"
+                    >
+                        Publish on timeline
+                    </b-form-checkbox>
                     <b-form-group label="Video URL : " class="text-left">
-                        <b-form-radio v-model="selectedVideoUrlSource" name="video-url-radios" value="Youtube">Youtube
+                        <b-form-radio v-model="form.video_type" name="video-url-radios" value="youtube">Youtube
                         </b-form-radio>
-                        <b-form-radio v-model="selectedVideoUrlSource" name="video-url-radios" value="Vimeo">Vimeo
+                        <b-form-radio v-model="form.video_type" name="video-url-radios" value="vimeo">Vimeo
                         </b-form-radio>
-                        <b-form-radio v-model="selectedVideoUrlSource" name="video-url-radios" value="Other Link">Others
+                        <b-form-radio v-model="form.video_type" name="video-url-radios" value="other Link">Others
                         </b-form-radio>
                     </b-form-group>
-                    <div class="mt-3 text-left" v-if="selectedVideoUrlSource !== null">Video URL Source: <strong>{{selectedVideoUrlSource}}</strong>
+                    <div class="mt-3 text-left" v-if="form.video_type !== null">Video URL Source: <strong>{{form.video_type}}</strong>
                     </div>
-                    <b-form-group v-if="selectedVideoUrlSource !== null"
+                    <b-form-group v-if="form.video_type !== null"
                                   id="video url"
                                   label-for="video-url"
                     >
                         <b-form-input
                                 id="video-url"
-                                v-model="form.videoUrl"
+                                v-model="form.video_url"
                                 type="text"
                                 required
                                 placeholder="Enter the video URL link"
@@ -66,7 +69,7 @@
                         <b-col>
                             <h6 class="text-left">Publish Date : </h6>
                             <datepicker class="text-left form-control" placeholder="Select Publish Date"
-                                        v-model="form.publishDate"></datepicker>
+                                        v-model="form.publish_date"></datepicker>
                         </b-col>
                     </b-row>
                     <b-row>
@@ -95,7 +98,7 @@
                     <b-row>
                         <b-col>
                             <h6 class="text-left">Publish it in : </h6>
-                            <b-form-select v-model="form.publishItIn" :options="publishItInOptions"></b-form-select>
+                            <b-form-select v-model="form.sharing_with" :options="publishItInOptions"></b-form-select>
                         </b-col>
                     </b-row>
                     <b-row>
@@ -103,7 +106,7 @@
                             <b-form-group label="Publish In Special Libraries:" class="text-left">
                                 <b-form-checkbox-group
                                         id="checkbox-group-1"
-                                        v-model="form.publishInSpecialLibraries"
+                                        v-model="form.publish_custom_library"
                                         :options="specialLibrariesOptions"
                                         name="flavour-1"
                                 ></b-form-checkbox-group>
@@ -128,7 +131,7 @@
 
                                 <b-form-checkbox-group
                                         id="classes"
-                                        v-model="selected"
+                                        v-model="form.students"
                                         :options="classes"
                                         name="classes"
                                         class="ml-4"
@@ -160,14 +163,20 @@
     data: () => ({
       form: {
         title: '',
-        allowDownloadForStudents: false,
-        description: '',
-        videoUrl: '',
-        publishDate: null,
+        can_students_download: false,
+        video_type: null,
+        video_file: null,
+        srt_file: null,
+        video_url: '',
+        publish_date: null,
+        publish_timeline: false,
         selectedPreparation: null,
         tags: [],
+        sharing_with: null,
         publishItIn: null,
-        publishInSpecialLibraries: [],
+        publish_custom_library: [],
+        students: [],
+        subject_id: 1
       },
       preparationsOptions: [],
       publishItInOptions: [
@@ -191,11 +200,9 @@
       specialLibrariesOptions: [],
       tag: '',
       classes: [],
-      selected: [],
       allSelected: false,
       indeterminate: false,
       userIds: [],
-      selectedVideoUrlSource: null,
       loading: true
     }),
     methods: {
@@ -204,7 +211,7 @@
         console.log(this.form);
       },
       toggleAll(checked) {
-        this.selected = checked ? this.classes.slice() : [];
+        this.form.students = checked ? this.classes.slice() : [];
       },
       fetchPreparations: function () {
         const params = {
@@ -213,7 +220,9 @@
         const preparationsBaseURI = 'https://roles.viewclass.com/api/preparing.list';
         this.$http.get(preparationsBaseURI, { params })
           .then((result) => {
-            this.preparationsOptions = Object.values(result.data[1]); // take only values from the object adn get me an array
+            console.log(result.data);
+            this.preparationsOptions = Object.values(result.data[1]);
+
             console.log('preparationsOptions : ', this.preparationsOptions);
           })
           .catch(error => {
